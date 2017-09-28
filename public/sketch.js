dfd78539a539e8d62da8524717000332a54477e3
Seth <sz16900@my.bristol.ac.uk>
2017-09-28 19:51:52 +0100

    Server with socets and clients sending and receiving messages.

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,37 @@
+var socket;
+
 function setup() {
-	
+	createCanvas(400, 400);
+	background(51);
+
+	socket = io.connect('http://localhost:3000');
+	// If client receives messaage with mouse, trogger function/
+	socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+	noStroke();
+	fill(255, 0, 100);
+	ellipse(data.x, data.y, 36, 36)
+}
+
+function mouseDragged() {
+	console.log("sending: " + mouseX + ',' + mouseY);
+
+	// Save the mouseX and mouseY value
+	var data = {
+		x: mouseX,
+		y:mouseY
+	}
+	// Name the message and attach the data
+	socket.emit('mouse', data);
+
+
+	noStroke();
+	fill(255);
+	ellipse(mouseX, mouseY, 36, 36)
+}
+
+function draw() {
+
 }
